958bc8a3dba1dcc94da1cfb7226ba2732cfb88df
Jake Niman <illidans911@gmail.com>
2013-12-02 19:39:13 -0500

    Version 0.1 12/2/13-7:38
    Contributions : Jake Niman
    Branch Merges : None

---
+++ org/formula/root/FormulaParser.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.formula.root;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Arhowk
+ */
+public class FormulaParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    final String plus = "+";
+    final String minus = "-";
+    final String times = "*";
+    final String div = "/";
+    final String expo = "^";
+    final String mod = "%";
+    final String oper = "(";
+    final String cper = ")";
+    
+    String omniFormula;
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Input a formula where independant variable is x and dependant variable is y");
+        System.out.print("     ");
+        
+        
+    }
+}
